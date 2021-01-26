d9b8d944271d4d09c4f9dfc2359f8f2ef900b69b
Mitchell Bean <mitchtroybean@gmail.com>
2021-01-25 22:04:25 -0800

    orm and burger.js

---
+++ controllers/burgers_Controller.js
@@ -0,0 +1,8 @@
+//dependencies
+var express = require("express");
+var burger = require("../models/burger.js");
+
+var router = express.Router();
+
+// Export routes for server.js to use.
+module.exports = router;
