7601877d8514cc14a15ac357edab6e71f2089bb9
Mitchell Bean <mitchtroybean@gmail.com>
2021-01-26 22:23:24 -0800

    boiler plate for heroku

--- config/connection.js
+++ config/connection.js
@@ -1,13 +1,18 @@
 // Set up MySQL connection.
-var mysql = require("mysql");
+const mysql = require("mysql");
 
-var connection = mysql.createConnection({
-  host: "localhost",
-  port: 3306,
-  user: "root",
-  password: "password",
-  database: "burgers_db",
-});
+let connection;
+if (process.env.JAWSDB_URL) {
+  connection = myql.createConnection(process.env.JAWSDB_URL);
+} else {
+  let connection = mysql.createConnection({
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "password",
+    database: "burgers_db",
+  });
+}
 
 // Make connection.
 connection.connect(function (err) {
